6b8c1a4ca6d8cc76cc980a6c5c40bfd7601f7cc7
Chris Chitla <chrischitla@gmail.com>
2019-09-11 12:57:03 -0700

    added jquery to append attacker and defender stats when they are clicked(selected)

--- assets/javascript/rpgnormal.js
+++ assets/javascript/rpgnormal.js
@@ -2,24 +2,28 @@ $(document).ready(function () {
 
 
     let luke = {
+        name: "luke",
         hp: 120,
         ap: 20,
         cp: 15,
     }
 
     let han = {
+        name: "han",
         hp: 100,
         ap: 15,
         cp: 20
     }
 
     let vader = {
+        name: "vader",
         hp: 110,
         ap: 15,
         cp: 10
     }
 
     let palpatine = {
+        name: "palpatine",
         hp: 140,
         ap: 20,
         cp: 5
@@ -28,45 +32,83 @@ $(document).ready(function () {
 
     function displayChars() {
         let chooseLuke = $("<img>");
-        chooseLuke.attr({ "src": "assets/images/luke.jpg", "name": "luke" });
+        chooseLuke.attr({ "src": "assets/images/luke.jpg", "name": luke.name });
         chooseLuke.css({ "border": "3px solid black", "position": "relative", "width":"100%" });
-        chooseLuke.addClass("luke");
+        // chooseLuke.addClass("luke");
         $("#luke").append(chooseLuke);
 
         let chooseHan = $("<img>");
-        chooseHan.attr({ "src": "assets/images/han.jpg", "name": "han" });
+        chooseHan.attr({ "src": "assets/images/han.jpg", "name": han.name });
         chooseHan.css({ "border": "3px solid black", "position": "relative", "width":"100%" });
-        chooseHan.addClass("han");
+        // chooseHan.addClass("han");
         $("#han").append(chooseHan);
 
         let chooseVader = $("<img>");
-        chooseVader.attr({ "src": "assets/images/vader.jpg", "name": "vader" });
+        chooseVader.attr({ "src": "assets/images/vader.jpg", "name": vader.name });
         chooseVader.css({ "border": "3px solid black", "position": "relative", "width":"100%" });
-        chooseVader.addClass("vader");
+        // chooseVader.addClass("vader");
         $("#vader").append(chooseVader);
 
         let choosePalpatine = $("<img>");
-        choosePalpatine.attr({ "src": "assets/images/palpatine.jpg", "name": "palpatine" });
+        choosePalpatine.attr({ "src": "assets/images/palpatine.jpg", "name": palpatine.name });
         choosePalpatine.css({ "border": "3px solid black", "position": "relative", "width":"100%" });
-        choosePalpatine.addClass("palpatine");
+        // choosePalpatine.addClass("palpatine");
         $("#palpatine").append(choosePalpatine);
 
     }
 
     displayChars();
 
+
    
 
-    $(".display-chars").mouseover(function(){
-        console.log($(this))
+    // mouse over stat info ===============================================================
+
+    $("#luke").mouseover(function() {
+        console.log($(this).attr("id"));
         let stats = $("<span>");
         stats.addClass("luke-stats");
         stats.text(`HP:${luke.hp} AP:${luke.ap} CP:${luke.cp}`);
         $("#luke-stats").append(stats);
     })
-    $(".display-chars").mouseout(function(){
+    $("#luke-stats").mouseout(function(){
         $(".luke-stats").remove();
     })
+
+    $("#han").mouseover(function() {
+        console.log($(this).attr("id"));
+        let stats = $("<span>");
+        stats.addClass("han-stats");
+        stats.text(`HP:${han.hp} AP:${han.ap} CP:${han.cp}`);
+        $("#han-stats").append(stats);
+    })
+    $("#han-stats").mouseout(function() {
+        $(".han-stats").remove();
+    })
+
+    $("#vader").mouseover(function() {
+        console.log($(this).attr("id"));
+        let stats = $("<span>");
+        stats.addClass("vader-stats");
+        stats.text(`HP:${vader.hp} AP:${vader.ap} CP:${vader.cp}`);
+        $("#vader-stats").append(stats);
+    })
+    $("#vader-stats").mouseout(function() {
+        $(".vader-stats").remove();
+    })
+
+    $("#palpatine").mouseover(function() {
+        console.log($(this).attr("id"));
+        let stats = $("<span>");
+        stats.addClass("palpatine-stats");
+        stats.text(`HP:${palpatine.hp} AP:${palpatine.ap} CP:${palpatine.cp}`);
+        $("#palpatine-stats").append(stats);
+    })
+    $("#palpatine-stats").mouseout(function() {
+        $(".palpatine-stats").remove();
+    })
+
+   // ===========================================================================================
 
 
 
@@ -96,26 +138,43 @@ $(document).ready(function () {
 
     })
 
-    //moves user character to position
+    //moves user character to position, displays their stats
     function charIsNotSelected(char) {
         $(char).addClass("attacker");
 
         if ($(char).attr("name") === "luke") {
             $(char).animate({ top: "+=335px" });
+            $("#attacker-hp").append(luke.hp);
+            $("#attacker-ap").append(luke.ap);
+            $("#attacker-cp").append(luke.cp);
         }
         else if (($(char).attr("name") === "han")) {
             $(char).animate({ top: "+=335px", right: "+=286px" });
+            $("#attacker-hp").append(han.hp);
+            $("#attacker-ap").append(han.ap);
+            $("#attacker-cp").append(han.cp);
         }
         else if (($(char).attr("name") === "vader")) {
             $(char).animate({ top: "+=335px", right: "+=570px" });
+            $("#attacker-hp").append(vader.hp);
+            $("#attacker-ap").append(vader.ap);
+            $("#attacker-cp").append(vader.cp);
         }
         else if (($(char).attr("name") === "palpatine")) {
             $(char).animate({ top: "+=335px", right: "+=855px" });
+            $("#attacker-hp").append(palpatine.hp);
+            $("#attacker-ap").append(palpatine.ap);
+            $("#attacker-cp").append(palpatine.cp);
         }
 
         userCharSelected = true;
     }
 
+
+
+
+
+    // when defender selected call move defender function =================================================
 
     function selectDefender1(opponent) {
         $(opponent).addClass("defender1");
@@ -142,17 +201,33 @@ $(document).ready(function () {
     function moveDefenders(opponent) {
         if ($(opponent).attr("name") === "luke") {
             $(opponent).animate({ top: "+=335px", left: "+=590px" });
+            $("#defender-hp").append(luke.hp);
+            $("#defender-ap").append(luke.ap);
+            $("#defender-cp").append(luke.cp);
         }
         else if (($(opponent).attr("name") === "han")) {
             $(opponent).animate({ top: "+=335px", left: "+=303px" });
+            $("#defender-hp").append(han.hp);
+            $("#defender-ap").append(han.ap);
+            $("#defender-cp").append(han.cp);
         }
         else if (($(opponent).attr("name") === "vader")) {
             $(opponent).animate({ top: "+=335px", left: "+=20px" });
+            $("#defender-hp").append(vader.hp);
+            $("#defender-ap").append(vader.ap);
+            $("#defender-cp").append(vader.cp);
         }
         else if (($(opponent).attr("name") === "palpatine")) {
             $(opponent).animate({ top: "+=335px", right: "+=264px" });
+            $("#defender-hp").append(palpatine.hp);
+            $("#defender-ap").append(palpatine.ap);
+            $("#defender-cp").append(palpatine.cp);
         }
     }
+
+    // ==========================================================================================================
+
+
 
 
 
